1a3d0b0f8323e29b83f01aa643698ef354cbdf6e
root <root@rofl.conova.net>
2019-06-03 13:39:22 +0200

    add schema for postgres including correct constraints

---
+++ schema/pg_slaperiods.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS icinga_sla_periods;
+CREATE TABLE icinga_sla_periods (
+  timeperiod_object_id BIGINT NOT NULL,
+  start_time timestamp NOT NULL,
+  end_time timestamp NULL DEFAULT NULL,
+);
+ALTER TABLE icinga_sla_periods ADD constraint tp_start PRIMARY KEY (timeperiod_object_id,start_time);
+ALTER TABLE icinga_sla_periods ADD constraint tp_end UNIQUE(timeperiod_object_id,end_time);
+
+DROP TABLE IF EXISTS icinga_outofsla_periods;
+CREATE TABLE icinga_outofsla_periods (
+  timeperiod_object_id BIGINT NOT NULL,
+  start_time timestamp NOT NULL,
+  end_time timestamp NULL DEFAULT NULL
+) ;
+ALTER TABLE icinga_outofsla_periods ADD constraint tp_start PRIMARY KEY (timeperiod_object_id,start_time);
+ALTER TABLE icinga_outofsla_periods ADD constraint tp_end UNIQUE(timeperiod_object_id,end_time);
