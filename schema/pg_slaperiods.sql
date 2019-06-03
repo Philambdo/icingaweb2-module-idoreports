7e83c44fc5bf066c2f77c03b29a72ac5d8a42796
root <root@rofl.conova.net>
2019-06-03 13:42:55 +0200

    constraints uniquenes

--- schema/pg_slaperiods.sql
+++ schema/pg_slaperiods.sql
@@ -13,5 +13,5 @@ CREATE TABLE icinga_outofsla_periods (
   start_time timestamp NOT NULL,
   end_time timestamp NULL DEFAULT NULL
 ) ;
-ALTER TABLE icinga_outofsla_periods ADD constraint tp_start PRIMARY KEY (timeperiod_object_id,start_time);
-ALTER TABLE icinga_outofsla_periods ADD constraint tp_end UNIQUE(timeperiod_object_id,end_time);
+ALTER TABLE icinga_outofsla_periods ADD constraint outofsla_tp_start PRIMARY KEY (timeperiod_object_id,start_time);
+ALTER TABLE icinga_outofsla_periods ADD constraint outofsla_tp_end UNIQUE(timeperiod_object_id,end_time);
